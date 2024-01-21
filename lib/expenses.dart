442705dc6973eb6093754e9fefe3bdadcfe3905a
David Chen <chen.david.1016@gmail.com>
2024-01-21 01:07:42 -0600

    display list of expenses from dummy data

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_expense_tracker/expenses_list.dart';
+import 'package:flutter_expense_tracker/models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -10,14 +12,34 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Coffee',
+      amount: 8.55,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: 'Gas',
+      amount: 37.89,
+      date: DateTime.now(),
+      category: Category.travel,
+    ),
+    Expense(
+      title: 'Insurance',
+      amount: 200.00,
+      date: DateTime.now(),
+      category: Category.work,
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text('Chart'),
-          Text('Expenses List'),
+          const Text('Chart'),
+          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
         ],
       ),
     );
