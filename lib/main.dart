72dd9261b719598d6db843665a63fc8cc3a77e47
David Chen <chen.david.1016@gmail.com>
2024-01-26 16:40:35 -0600

    added dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter_expense_tracker/widgets/expenses.dart';
 
 var kColorScheme =
-    ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 59, 138, 181));
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 59, 138, 181));
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 158, 112, 231),
+  brightness: Brightness.dark,
+);
 
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+            color: kDarkColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6)),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+                backgroundColor: kDarkColorScheme.primaryContainer)),
+      ),
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
@@ -24,6 +38,7 @@ void main() {
                 color: kColorScheme.onSecondaryContainer,
                 fontSize: 18)),
       ),
+      themeMode: ThemeMode.system,
       home: const Expenses(),
     ),
   );
