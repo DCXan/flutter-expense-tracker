fb6cf208fa2bafcd35d6acf78a98ac8855cab1c1
David Chen <chen.david.1016@gmail.com>
2024-01-26 16:22:53 -0600

    apply theme data

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ void main() {
                 backgroundColor: kColorScheme.primaryContainer)),
         textTheme: ThemeData().textTheme.copyWith(
             titleLarge: TextStyle(
-                fontWeight: FontWeight.normal,
+                fontWeight: FontWeight.bold,
                 color: kColorScheme.onSecondaryContainer,
                 fontSize: 18)),
       ),
